0b4544955b9286d98d570c5eeee669b823bbe11f
Hazar NAMDAR <hazarnamdarrr@gmail.com>
2024-05-11 22:10:28 +0300

    fix(core): repo updated for windows distro

--- main.cpp
+++ main.cpp
@@ -1,75 +1,26 @@
 #include <iostream>
-#include <vector>
-#include <chrono>
-#include <math.h>
-#include <time.h>
+#include <unordered_map>
 
 using namespace std;
 
-double pow(double, int);
-void test_pow(vector<double>&, vector<int>&);
-void initialize_testValues(vector<double>&, vector<int>&);
+double power(double, int);
 
 int main() {
-    srand(time(NULL));
+    double a; int n;
 
-    vector<double> bases;
-    vector<int> exps;
-    test_pow(bases, exps);
-    
+    cout << "Enter a: "; cin >> a;
+    cout << "Enter n: "; cin >> n;
+    cout << a << " raised to the power of " << n << " is: " << power(a, n) << endl;
+
     return 0;
 }
 
-double myPow(double a, int n) {
+double power(double a, int n) {
     if (n == 0) return 1;
     if (n == 1) return a;
 
-    if (n & 1) return a * myPow(a * a, (n - 1) / 2);
-    else return myPow(a * a, n / 2);
-}
-
-void initialize_testValues(vector<double>& bases, vector<int>& exps) {
-    for (int i = 0; i < 35; i++) {
-        bases.push_back(0.0 + 20.0 * (static_cast<double>(rand()) / RAND_MAX));
-        exps.push_back(rand() % 100);
-    }
-}
-void test_pow(vector<double>& bases, vector<int>& exps) { 
-    initialize_testValues(bases, exps);
-
-    auto lib_start = chrono::high_resolution_clock::now();
-
-    for (int i = 0; i < bases.size(); i++) 
-        pow(bases[i], (double) exps[i]);
-
-    auto lib_end = chrono::high_resolution_clock::now();
-
-    auto lib_duration = chrono::duration_cast<chrono::microseconds>(lib_end - lib_start);
-
-    auto my_start = chrono::high_resolution_clock::now();
-
-    for (int i = 0; i < bases.size(); i++) 
-        myPow(bases[i], exps[i]);
-
-    auto my_end = chrono::high_resolution_clock::now();
-
-    auto my_duration = chrono::duration_cast<chrono::microseconds>(my_end - my_start);
-
-    cout << "Library pow() took -> " << lib_duration.count() << " ms" << endl;
-    cout << "My pow() took      -> " << my_duration.count() << " ms" << endl;
-
-    bool passed = true;
-    for (int i = 0; i < bases.size(); i++) {
-        double l = pow(bases[i], (double) exps[i]);
-        double m = myPow(bases[i], exps[i]);
-
-        // How many errors in 10 quadrillion
-        double err = (abs(l - m) / l) * 10000000000000000.0;
-
-        if (err > 100) {
-            passed = false;
-        }
-    }
-
-    if (passed) cout << "All tests are passed!" << endl;
+    if (n < 0)  return 1 / power(a, -n);
+    
+    if (n & 1)  return a * power(a * a, (n - 1) / 2);
+    else        return power(a * a, n / 2);
 }
